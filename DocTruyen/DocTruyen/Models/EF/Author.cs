1942f7b36f2925051bfdff8fbe468fa66878113b
tri63132732 <144207384+tri63132732@users.noreply.github.com>
2024-05-24 21:29:45 +0700

    Update

---
+++ DocTruyen/DocTruyen/Models/EF/Author.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace DocTruyen.Models.EF
+{
+    [Table("tb_Author")]
+    public class Author: Common
+    {
+        public Author() {
+            this.StoryAuthors = new HashSet<StoryAuthor>();
+        }
+        [Key]
+        [DatabaseGeneratedAttribute(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+        [Required(ErrorMessage = "Tên tác giả không để trống")]
+        [StringLength(150)]
+        public string Name { get; set; }
+        public string Alias { get; set; }
+        public string Description { get; set; }
+        public string Keyword { get; set; }
+        public virtual ICollection<StoryAuthor> StoryAuthors { get; set; }
+    }
+}
---
+++ DocTruyen/DocTruyen/Models/EF/Chapter.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace DocTruyen.Models.EF
+{
+    [Table("tb_Chapter")]
+    public class Chapter: Common
+    {
+        [Key]
+        [DatabaseGeneratedAttribute(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+        [Required(ErrorMessage = "Tên chương không để trống")]
+        [StringLength(150)]
+        public string Name { get; set; }
+        public string Alias { get; set; }
+        public string Content { get; set; }
+        public string Keyword { get; set; }
+        public int StoryId { get; set; }
+        public virtual Story Story { get; set; }
+    }
+}
